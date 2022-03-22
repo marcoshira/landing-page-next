a1e17ffadf23cc33b7d97882f43958d4aceb7a8e
Marcos Hirazawa <hirazawa.marcos@gmail.com>
2022-03-22 20:29:14 -0300

    husky fail

--- src/components/TextComponent/styles.js
+++
@@ -1,8 +0,0 @@
-import styled, { css } from 'styled-components';
-
-export const Container = styled.div`
-  ${({ theme }) => css`
-    font-size: ${theme.font.sizes.medium};
-    padding: 0rem;
-  `}
-`;
